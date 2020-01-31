dc36c842150328a790dd6f399646cf36aec83cdd
Trout Lake School First Robotics Team <robotics@tlschool.net>
2020-01-30 18:52:51 -0800

    add pigeonimu to shooter subsystem for testing

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,12 +1,28 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase{
     private WPI_TalonSRX shooter1Controller = new WPI_TalonSRX(05); 
     private WPI_TalonSRX shooter2Controller = new WPI_TalonSRX(27); 
+
+    PigeonIMU pigeonIMU = new PigeonIMU(shooter2Controller);
+    
+    
+    public ShooterSubsystem(){
+        addChild("shooter1",shooter1Controller);
+        addChild("shooter2",shooter2Controller);
+        //addChild("pigeon",pigeonIMU);
+    }
+
+    @Override
+    public void periodic() {
+       SmartDashboard.putNumber("pigeon fused heading", pigeonIMU.getFusedHeading());
+    }
 
     public void enableShooter(boolean enable)
     {
