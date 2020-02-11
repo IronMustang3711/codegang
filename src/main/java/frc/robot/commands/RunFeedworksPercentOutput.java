893e7350a6215d1244461e82ca37b752a0bb4dc5
jasonnn <jasonmarkham@gmail.com>
2020-02-11 15:49:52 -0800

    %output run commands for feedworks and infeed

---
+++ src/main/java/frc/robot/commands/RunFeedworksPercentOutput.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.FeederSubsystem;
+
+public class RunFeedworksPercentOutput extends CommandBase {
+  final FeederSubsystem feedworks;
+  final double pctOut1;
+  final double pctOut2;
+
+  public RunFeedworksPercentOutput(FeederSubsystem feedworks, double pctOut1, double pctOut2) {
+    this.feedworks = feedworks;
+    this.pctOut1 = pctOut1;
+    this.pctOut2 = pctOut2;
+    addRequirements(feedworks);
+  }
+
+  @Override
+  public void execute() {
+    feedworks.controller1.set(pctOut1);
+    feedworks.controller2.set(pctOut2);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    feedworks.controller1.neutralOutput();
+    feedworks.controller2.neutralOutput();
+  }
+}
