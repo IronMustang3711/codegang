63326ce77720b6b6f6b8be458b3baf6b1179d1ad
jasonnn <jasonmarkham@gmail.com>
2020-02-10 18:21:59 -0800

    PizzaWheelSubsytem wip

--- src/main/java/frc/robot/subsystems/PizzaWheelSubsytem.java
+++ src/main/java/frc/robot/subsystems/PizzaWheelSubsytem.java
@@ -3,19 +3,33 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PizzaWheelSubsytem extends SubsystemBase {
 
   WPI_TalonSRX turner = new WPI_TalonSRX(0);
-  WPI_TalonSRX angler = new WPI_TalonSRX(86);
+  Talon angler = new Talon(0);
   ColorSensorV3 colorSensor = new ColorSensorV3(I2C.Port.kMXP);
 
   public PizzaWheelSubsytem() {
     setName("PizzaWheel");
     addChild("turner", turner);
     addChild("angler", angler);
+
+    setupShuffleboardTab();
+
+  }
+
+  void setupShuffleboardTab() {
+    ShuffleboardTab tab = Shuffleboard.getTab(PizzaWheelSubsytem.class.getSimpleName());
+    tab.add(turner);
+    tab.add(angler);
+
+    tab.addNumber("pizza encoder", () -> (double) turner.getSelectedSensorPosition());
   }
 
   public void periodic() {
