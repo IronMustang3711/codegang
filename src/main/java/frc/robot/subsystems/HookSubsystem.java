5120029cc16d0c78c12ff957f23b5e4fdea5d41c
jasonnn <jasonmarkham@gmail.com>
2020-02-13 00:32:28 -0800

    hook config **testme**

--- src/main/java/frc/robot/subsystems/HookSubsystem.java
+++ src/main/java/frc/robot/subsystems/HookSubsystem.java
@@ -6,13 +6,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import frc.robot.Constants.TalonConstants;
 import frc.robot.commands.ResetSensors;
 import frc.robot.stuff.SensorReset;
-
-import java.util.List;
 
 public class HookSubsystem extends PIDSubsystem implements SensorReset {
   private WPI_TalonSRX hookController;
@@ -38,8 +35,13 @@ public class HookSubsystem extends PIDSubsystem implements SensorReset {
     hookController.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,
                                                 TalonConstants.PRIMARY_PID,
                                                 TalonConstants.DEFAULT_TIMEOUT);
+    hookController.selectProfileSlot(TalonConstants.PRIMARY_SLOT, TalonConstants.PRIMARY_PID);
     hookController.setSelectedSensorPosition(0, TalonConstants.PRIMARY_PID,
                                              TalonConstants.DEFAULT_TIMEOUT);
+
+    hookController.configVoltageCompSaturation(10.0, TalonConstants.DEFAULT_TIMEOUT);
+    hookController.configPeakOutputForward(0.5, TalonConstants.DEFAULT_TIMEOUT);
+    hookController.configPeakOutputReverse(0.3, TalonConstants.DEFAULT_TIMEOUT);
 
     setupShuffleboard();
 
@@ -50,6 +52,7 @@ public class HookSubsystem extends PIDSubsystem implements SensorReset {
     tab.add(getController());
     tab.add(hookController);
     tab.addNumber("hookPosition", this::getEncoderPosition);
+    tab.addNumber("hookVelocity", this::getEncoderVelocity);
     tab.addNumber("hookCurrent", hookController::getStatorCurrent);
     tab.add(new ResetSensors<>(this));
 
@@ -66,8 +69,6 @@ public class HookSubsystem extends PIDSubsystem implements SensorReset {
   @Override
   public void periodic() {
     super.periodic();
-    // SmartDashboard.putNumber("hook_position", getEncoderPosition());
-    // SmartDashboard.putNumber("hook_velocity", getEncoderVelocity());
   }
 
   @Override
