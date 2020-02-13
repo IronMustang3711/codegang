1e72e7acf4b21c4acf0664231797cca362249f50
jasonnn <jasonmarkham@gmail.com>
2020-02-12 23:43:42 -0800

    ShooterVelocityControl telem

--- src/main/java/frc/robot/commands/ShooterVelocityControl.java
+++ src/main/java/frc/robot/commands/ShooterVelocityControl.java
@@ -2,15 +2,14 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterVelocityControl extends CommandBase {
   final ShooterSubsystem shooter;
-  private final NetworkTableEntry setpoint;
   private final NetworkTableEntry error1;
   private final NetworkTableEntry output1;
   private final NetworkTableEntry error2;
@@ -19,13 +18,20 @@ public class ShooterVelocityControl extends CommandBase {
   public ShooterVelocityControl(ShooterSubsystem shooter) {
     this.shooter = shooter;
     addRequirements(shooter);
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("shooter");
-    this.setpoint = table.getEntry("setpoint");
-    setpoint.setDefaultDouble(1000.0);
-    error1 = table.getEntry("error1");
-    output1 = table.getEntry("output1");
-    error2 = table.getEntry("error2");
-    output2 = table.getEntry("output2");
+
+
+    shooter.setpoint.setDefaultDouble(1000.0);
+
+    var tab = Shuffleboard.getTab(ShooterSubsystem.class.getSimpleName());
+    var errors = tab.getLayout("error", BuiltInLayouts.kList);
+    error1 = errors.add("error1", 0.0).getEntry();
+    error2 = errors.add("error2", 0.0).getEntry();
+
+    var outs = tab.getLayout("output%");
+    output1 = outs.add("output1", 0.0).getEntry();
+    output2 = outs.add("output2", 0.0).getEntry();
+
+
   }
 
   @Override
@@ -36,16 +42,17 @@ public class ShooterVelocityControl extends CommandBase {
     shooter.controller1.config_kP(0, 0.01);
     shooter.controller1.configAllowableClosedloopError(0, 100);
 
-    shooter.controller1.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
-    shooter.controller1.configVoltageCompSaturation(10.0);
-    shooter.controller1.config_kF(0, 0.04);
-    shooter.controller1.config_kP(0, 0.01);
-    shooter.controller1.configAllowableClosedloopError(0, 100);
+    shooter.controller2.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    shooter.controller2.configVoltageCompSaturation(10.0);
+    shooter.controller2.config_kF(0, 0.04);
+    shooter.controller2.config_kP(0, 0.01);
+    shooter.controller2.configAllowableClosedloopError(0, 100);
   }
 
   @Override
   public void execute() {
-    double sp = setpoint.getDouble(0.0);
+    double sp = shooter.setpoint.getDouble(0.0);
+
     shooter.controller1.set(ControlMode.Velocity, sp);
     shooter.controller2.set(ControlMode.Velocity, sp);
 
